64e55abb4c7c706cb0cbd4f7a2baa70042467a3c
pluto <pluto@pluto-VirtualBox.(none)>
2015-12-09 22:43:27 +0800

    show button

--- user.php
+++ user.php
@@ -1,52 +1,59 @@
-<?php
- echo("<title>user mode</title>");
-?>
+
+<?echo("<title>user mode</title>");?>
+
 <script>
+   funcion info() {
 
- funcion info() {
-
-    var p = location.search.substring(1).split("=");
-    p = unescape(p[1]);
-    var a = p.split("+");
-    for(var i=0;i<a.length;i=i+1){
-        document.write(a[i]+" ");
-        }
+	var p = location.search.substring(1).split("=");
+	p = unescape(p[1]);
+	var a = p.split("+");
+	for(var i=0;i<a.length;i=i+1){
+		document.write(a[i]+" ");
+	}
 }
 </script>
+
+<p><b><font size=6>R442's Property</font></b></p>
 <p>
 <?
-   
-   $link=mysql_connect("localhost","root","1234");
-   if(!$link){
+
+$link=mysql_connect("localhost","root","1234");
+if(!$link){
 	die("fail : ".mysql_error());
-   }
-   
-   $db_selected=mysql_select_db("object",$link);
-   if(!$db_selected){
+}
+
+$db_selected=mysql_select_db("object",$link);
+if(!$db_selected){
 	die("db fail :".mysql_error($link));	
-    }	
-   mysql_query("SET NAMES 'utf8'"); 	  
-   $sql="SELECT * FROM o";
-   $result = mysql_query($sql) ;
-   $total_records=mysql_num_rows($result); 
+}	
+mysql_query("SET NAMES 'utf8'"); 	  
+$sql="SELECT * FROM o";
+$result = mysql_query($sql) ;
+$total_records=mysql_num_rows($result); 
 ?>
 </p>
-    <TABLE BORDER='1' ALIGN='CENTER'><TR ALIGN='CENTER'>	
-  <tr>
-    <td>&nbsp&nbsp Item</td>
-    <td>&nbsp&nbsp Id  </td>
-    <td>&nbsp&nbsp Location&nbsp  </td>
-    <td>&nbsp&nbsp User &nbsp </td>
-    <td>&nbsp Action</td>	
- </tr>
-<? for ($i=0;$i<$total_records;$i++) {$row = mysql_fetch_array($result); ?> 
-  <tr>
-<td>&nbsp&nbsp<? echo $row[item];?>&nbsp&nbsp&nbsp</td> 
-  <td>&nbsp&nbsp<? echo $row[id];?>&nbsp&nbsp</td> 
-  <td>&nbsp&nbsp<? echo $row[location];?></td>
-  <td>&nbsp&nbsp<? echo $row[user];?>&nbsp&nbsp</td> 
-  <td><input type="button" onclick="php_function()" value="borrow"></td>
-</tr> 
-		  
- <?} ?>
+
+<table BORDER='1' ALIGN='CENTER'><tr ALIGN='CENTER'>	
+<tr>
+<td><b>&nbsp&nbsp Item</b></td>
+<td><b>&nbsp&nbsp Id</b></td>
+<td><b>&nbsp Location &nbsp</b></td>
+<td><b>&nbsp User &nbsp</b></td>
+<td><b>&nbsp Action</b></td>	
+</tr>
+<? for ($i=0;$i<$total_records;$i++) {$row = mysql_fetch_array($result);?> 
+   <tr>
+	<td>&nbsp&nbsp<? echo $row[item];?>&nbsp&nbsp&nbsp</td> 
+	<td>&nbsp&nbsp<? echo $row[id];?>&nbsp&nbsp</td> 
+	<td>&nbsp&nbsp<? echo $row[location];?></td>
+	<td>&nbsp&nbsp<? echo $row[user];?>&nbsp&nbsp</td>
+	<?if($row[user]==NULL){?>
+ 		<td><input type="button" onclick="info()" id=<? echo$row[id];?> value=Borrow></td>	
+        <?}else{ ?>
+	<td></td>
+	<?}?>
+  </tr> 
+
+<?} ?>
 </table>
+
