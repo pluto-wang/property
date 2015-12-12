e681ca3cfe921d0981fc5efefd2560d1e8921b3f
pluto <pluto@pluto-VirtualBox.(none)>
2015-12-12 23:24:11 +0800

    new window and show info

--- user.php
+++ user.php
@@ -16,8 +16,9 @@ if(!$db_selected){
 }	
 mysql_query("SET NAMES 'utf8'"); 	  
 $sql="SELECT * FROM o";
-$result = mysql_query($sql) ;
-$total_records=mysql_num_rows($result); 
+$result1 = mysql_query($sql) ;
+$result2 = mysql_query($sql) ;
+$total_records=mysql_num_rows($result1); 
 ?>
 
 <script language="javascript" type="application/javascript">
@@ -32,12 +33,27 @@ $total_records=mysql_num_rows($result);
 }
 
  var myWindow;
- function bw(){
+ function bw(id){
     
-    myWindow = window.open("", "", "width=300, height=300");
-    myWindow.document.write("<p>This is 'myWindow'</p>");
+	myWindow = window.open("", "Borrow", "width=400, height=300");
+	myWindow.document.write("<head><title>Borrow</title></head>");
+	myWindow.document.write("<p><b><font size=5>The information of your borrow is as follow:</font></b></p>");
+	<? for ($i=0;$i<$total_records;$i++){ $r=mysql_fetch_array($result1);?>
+		var d ='<?echo $r[id];?>';
+		if(d==id){
+			var item ='<?echo $r[item];?>';
+			myWindow.document.write("ITEM : ");
+			myWindow.document.write(item+" <br>");
 
-}
+			myWindow.document.write("ID : ");
+			myWindow.document.write(id+" <br>");
+			var date=new Date();
+			myWindow.document.write("DATE : ");
+			myWindow.document.write(date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate());		
+		}
+
+	<?}?>
+ }
 
 </script>
 
@@ -50,14 +66,14 @@ $total_records=mysql_num_rows($result);
 <td><b>&nbsp User &nbsp</b></td>
 <td><b>&nbsp Action</b></td>	
 </tr>
-<? for ($i=0;$i<$total_records;$i++) {$row = mysql_fetch_array($result);?> 
+<? for ($i=0;$i<$total_records;$i++) {$row = mysql_fetch_array($result2);?> 
    <tr>
 	<td>&nbsp&nbsp<? echo $row[item];?>&nbsp&nbsp&nbsp</td> 
 	<td>&nbsp&nbsp<? echo $row[id];?>&nbsp&nbsp</td> 
 	<td>&nbsp&nbsp<? echo $row[location];?></td>
 	<td>&nbsp&nbsp<? echo $row[user];?>&nbsp&nbsp</td>
 	<?if($row[user]==NULL){?>
- 		<td><input type="button" onclick="bw()" id=<? echo$row[id];?> value=Borrow></td>	
+	<td><input type="button" onclick="bw('<?echo $row[id];?>')" value=Borrow></td>	
         <?}else{ ?>
 	<td></td>
 	<?}?>
